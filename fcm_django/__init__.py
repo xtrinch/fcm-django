70bb5edb34e73605c547d2fc4204c6bb32308bf5
vagrant <vagrant@localove>
2016-10-03 08:49:15 +0000

    new version

--- fcm_django/__init__.py
+++ fcm_django/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "xTrinch"
 __email__ = "mojca.rojko@gmail.com"
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 
 
 class NotificationError(Exception):
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import fcm_django
 from distutils.core import setup
 import setuptools
 
-VERSION = '0.1.8'
+VERSION = '0.1.9'
 
 CLASSIFIERS = [
 	"Development Status :: 5 - Production/Stable",
