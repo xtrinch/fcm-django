8d5e16b2b51d2fa037302a1cc331d3431abd710f
gonczor <wiktor.gonczaronek@gmail.com>
2017-11-28 14:58:59 +0100

    requirements and styles updated

--- setup.py
+++ setup.py
@@ -1,46 +1,44 @@
 #!/usr/bin/env python
-import os.path
 import fcm_django
 from distutils.core import setup
-import setuptools
 
 VERSION = '0.2.11'
 
 CLASSIFIERS = [
-	"Development Status :: 5 - Production/Stable",
-	"Environment :: Web Environment",
-	"Framework :: Django",
-	"Intended Audience :: Developers",
-	"License :: OSI Approved :: MIT License",
-	"Programming Language :: Python",
-	"Programming Language :: Python :: 2.7",
-	"Programming Language :: Python :: 3",
-	"Programming Language :: Python :: 3.4",
-	"Programming Language :: Python :: 3.5",
-	"Topic :: Software Development :: Libraries :: Python Modules",
-	"Topic :: System :: Networking",
+    "Development Status :: 5 - Production/Stable",
+    "Environment :: Web Environment",
+    "Framework :: Django",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: System :: Networking",
 ]
 
 setup(
-	name="fcm-django",
-	packages=[
-		"fcm_django",
-		"fcm_django/api",
-		"fcm_django/migrations",
-		"fcm_django/management",
-		"fcm_django/management/commands",
-	],
+    name="fcm-django",
+    packages=[
+        "fcm_django",
+        "fcm_django/api",
+        "fcm_django/migrations",
+        "fcm_django/management",
+        "fcm_django/management/commands",
+    ],
     install_requires=[
-        'pyfcm==1.3.1',
+        'pyfcm==1.4.3',
         'Django'
     ],
-	author=fcm_django.__author__,
-	author_email=fcm_django.__email__,
-	classifiers=CLASSIFIERS,
-	description="Send push notifications to mobile devices & browsers through "
-				"FCM in Django.",
-	download_url="https://github.com/xtrinch/fcm-django/tarball/master",
-	long_description='',
-	url="https://github.com/xtrinch/fcm-django",
-	version=fcm_django.__version__,
+    author=fcm_django.__author__,
+    author_email=fcm_django.__email__,
+    classifiers=CLASSIFIERS,
+    description="Send push notifications to mobile devices & browsers through "
+                "FCM in Django.",
+    download_url="https://github.com/xtrinch/fcm-django/tarball/master",
+    long_description='',
+    url="https://github.com/xtrinch/fcm-django",
+    version=fcm_django.__version__,
 )
