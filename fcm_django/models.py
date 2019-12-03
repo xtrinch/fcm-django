80ef9ccda7e571987ba4dea900e167660bc67e05
Alexandre Sanches <rolexsanches@gmail.com>
2019-12-03 14:18:37 -0300

    Remove Django 3.0 Incompatibility

--- fcm_django/models.py
+++ fcm_django/models.py
@@ -1,13 +1,11 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 from .settings import FCM_DJANGO_SETTINGS as SETTINGS
 
 
-@python_2_unicode_compatible
 class Device(models.Model):
     name = models.CharField(
         max_length=255,
