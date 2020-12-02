cb124a7e95b706873335d06e6372dbcd4cf3e16b
Mojca Rojko <mojca.rojko@gmail.com>
2020-12-02 20:22:29 +0100

    Bump version

--- fcm_django/__init__.py
+++ fcm_django/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "xTrinch"
 __email__ = "mojca.rojko@gmail.com"
-__version__ = "0.3.5"
+__version__ = "0.3.6"
 
 
 class NotificationError(Exception):
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-VERSION = '0.3.5'
+VERSION = '0.3.6'
 
 CLASSIFIERS = [
     "Development Status :: 5 - Production/Stable",
