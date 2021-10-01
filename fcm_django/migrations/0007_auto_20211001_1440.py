4d402b52e1d72e7891fb298c897797b75a725486
Andrew Chen Wang <60190294+Andrew-Chen-Wang@users.noreply.github.com>
2021-10-01 09:46:52 -0400

    Fix parameters

--- fcm_django/migrations/0007_auto_20211001_1440.py
+++ fcm_django/migrations/0007_auto_20211001_1440.py
@@ -11,11 +11,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name="fcmdevice",
             name="id",
-            field=models.BigAutoField(
-                auto_created=True,
-                primary_key=True,
-                serialize=False,
-                verbose_name="ID",
-            ),
+            field=models.BigAutoField(primary_key=True, serialize=False),
         ),
     ]
