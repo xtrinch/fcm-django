afb9e7885252a6fc93fcf177006055aae546fe7f
Andrew Chen Wang <60190294+Andrew-Chen-Wang@users.noreply.github.com>
2021-10-01 09:44:09 -0400

    Create migrations file

---
+++ fcm_django/migrations/0007_auto_20211001_1440.py
@@ -0,0 +1,21 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("fcm_django", "0006_auto_20210802_1140"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="fcmdevice",
+            name="id",
+            field=models.BigAutoField(
+                auto_created=True,
+                primary_key=True,
+                serialize=False,
+                verbose_name="ID",
+            ),
+        ),
+    ]
