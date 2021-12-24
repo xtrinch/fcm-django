cabab45d52509d83b6f5e749bb37f77fac37b758
xtrinch <mojca.rojko@gmail.com>
2021-12-24 14:43:13 +0100

    Format

--- fcm_django/migrations/0008_auto_20211224_1205.py
+++ fcm_django/migrations/0008_auto_20211224_1205.py
@@ -6,13 +6,15 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('fcm_django', '0007_auto_20211001_1440'),
+        ("fcm_django", "0007_auto_20211001_1440"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='fcmdevice',
-            name='id',
-            field=models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+            model_name="fcmdevice",
+            name="id",
+            field=models.AutoField(
+                auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+            ),
         ),
     ]
