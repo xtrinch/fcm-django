d0b24342923e2d5d5b7be6910ab7996d6f09dcec
Mojca Rojko <mojca.rojko@gmail.com>
2022-03-22 11:54:58 +0100

    Update models.py

--- fcm_django/models.py
+++ fcm_django/models.py
@@ -29,7 +29,7 @@ class Device(models.Model):
         help_text=_("Inactive devices will not be sent notifications"),
     )
     user = models.ForeignKey(
-        SETTINGS["USER_MODEL"], blank=True, null=True, on_delete=models.CASCADE
+        SETTINGS["USER_MODEL"], blank=True, null=True, on_delete=models.CASCADE, related_query_name=_("device")
     )
     date_created = models.DateTimeField(
         verbose_name=_("Creation date"), auto_now_add=True, null=True
