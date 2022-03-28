d9ebecfb9e7fca7e85a5477061fa2cd75e2cfce9
xtrinch <mojca.rojko@gmail.com>
2022-03-28 21:56:48 +0200

    Format

--- fcm_django/fields.py
+++ fcm_django/fields.py
@@ -7,7 +7,7 @@ from django.db import connection, models
 from django.utils.translation import gettext_lazy as _
 
 UNSIGNED_64BIT_INT_MIN_VALUE = 0
-UNSIGNED_64BIT_INT_MAX_VALUE = 2 ** 64 - 1
+UNSIGNED_64BIT_INT_MAX_VALUE = 2**64 - 1
 
 __all__ = ["HexadecimalField", "HexIntegerField"]
 
