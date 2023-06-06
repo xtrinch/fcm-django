c2345f2435032f0ffdf6241c074d0ff46f1d1ef0
olzhasar <o.arystanov@gmail.com>
2023-06-06 18:00:52 +0600

    Add tests for AbstractFCMDevice.send_topic_message

--- tests/test_models.py
+++ tests/test_models.py
@@ -23,24 +23,6 @@ def test_registration_id_size():
     device.save()
 
 
-@pytest.mark.django_db
-def test_firebase_raises(monkeypatch):
-    def _firebase_raises(*args, **kwargs):
-        raise FirebaseError(code=500, message="message")
-
-    monkeypatch.setattr("firebase_admin.messaging.send", _firebase_raises)
-
-    device = FCMDevice(
-        registration_id="test",
-        type=DeviceType.WEB,
-    )
-    with pytest.raises(FirebaseError):
-        device.send_message(message=Message())
-
-    with pytest.raises(FirebaseError):
-        device.send_topic_message(message=Message(), topic_name="topic")
-
-
 class TestFCMDeviceSendMessage:
     def assert_sent_successfully(
         self,
@@ -129,3 +111,87 @@ class TestFCMDeviceSendMessage:
         mock_fcm_device_deactivate.assert_called_once_with(
             fcm_device, fcm_device.registration_id, firebase_error
         )
+
+
+class TestFCMDeviceSendTopicMessage:
+    def assert_sent_successfully(
+        self,
+        result: Any,
+        message: Message,
+        topic: str,
+        mock_firebase_send: MagicMock,
+        message_id: str,
+        app: Any = None,
+        send_message_kwargs: Optional[dict] = None,
+    ):
+        send_message_kwargs = send_message_kwargs or {}
+
+        assert message.topic == topic
+
+        mock_firebase_send.assert_called_once_with(
+            message, app=app, **send_message_kwargs
+        )
+
+        # Ensure we properly construct the response with the exact same message that was
+        # obtained from messaging.send call
+        assert isinstance(result, SendResponse)
+        assert result.message_id == message_id
+
+    def test_ok(
+        self,
+        message: Message,
+        mock_firebase_send: MagicMock,
+        firebase_message_id_send: str,
+    ):
+        """
+        Ensure a topic message is being sent properly with default arguments
+        """
+        topic = "topic"
+
+        result = FCMDevice.send_topic_message(message, topic)
+
+        self.assert_sent_successfully(
+            result, message, topic, mock_firebase_send, firebase_message_id_send
+        )
+
+    def test_custom_params(
+        self,
+        message: Message,
+        mock_firebase_send: MagicMock,
+        firebase_message_id_send: str,
+    ):
+        """
+        Ensure custom firebase app and send_message_kwargs are being passed to
+        firebase_admin.messaging.send
+        """
+        topic = "topic"
+        custom_firebase_app = sentinel.CUSTOM_FIREBASE_APP
+        send_message_kwargs = {"foo": "bar"}
+
+        result = FCMDevice.send_topic_message(
+            message, topic, custom_firebase_app, **send_message_kwargs
+        )
+
+        self.assert_sent_successfully(
+            result,
+            message,
+            topic,
+            mock_firebase_send,
+            firebase_message_id_send,
+            custom_firebase_app,
+            send_message_kwargs,
+        )
+
+    def test_firebase_error(
+        self,
+        message: Message,
+        mock_firebase_send: MagicMock,
+        firebase_error: FirebaseError,
+    ):
+        """
+        Ensure we raise an error in case firebase_admin.messaging.send throws one
+        """
+        mock_firebase_send.side_effect = firebase_error
+
+        with pytest.raises(FirebaseError, match=str(firebase_error)):
+            FCMDevice.send_topic_message(message, "example")
